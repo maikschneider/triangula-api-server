f7445ef4ba40045e704b089587a45b7ff1c3e171
Maik Schneider <m.schneider@blueways.de>
2022-01-07 01:25:28 +0100

    feat: post image function

--- server.go
+++ server.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	"crypto/sha1"
+	"crypto/sha256"
 	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
 type Image struct {
@@ -20,15 +22,12 @@ type imageHandlers struct {
 	store map[string]Image
 }
 
-func (h *imageHandlers) get(w http.ResponseWriter, r *http.Request) {
+func (h *imageHandlers) loadStore() {
 	// read images directory
 	files, err := ioutil.ReadDir("images")
 	if err != nil {
 		panic(err.Error())
 	}
-
-	images := make([]Image, len(files))
-	i := 0
 
 	for _, file := range files {
 		// open file
@@ -37,20 +36,43 @@ func (h *imageHandlers) get(w http.ResponseWriter, r *http.Request) {
 			panic(err.Error())
 		}
 
-		// calc hash
-		hash := sha1.New()
-		if _, err := io.Copy(hash, f); err != nil {
-			panic(err)
+		// skip .gitkeep
+		if file.Name() == ".gitkeep" || file.IsDir() {
+			continue
+		}
+
+		// init hash
+		hash := sha256.New()
+
+		// get hash from filename (svg) or calculation
+		fileNameParts := strings.Split(file.Name(), ".")
+		fileHash := fileNameParts[0]
+		fileIsProcessed := true
+		if fileNameParts[1] != "svg" {
+			if _, err := io.Copy(hash, f); err != nil {
+				panic(err)
+			}
+			fileHash = base64.RawURLEncoding.EncodeToString(hash.Sum(nil))
+			fileIsProcessed = false
 		}
 
 		// construct single image response
-		images[i] = Image{
+		img := Image{
 			Name:      file.Name(),
-			Hash:      base64.URLEncoding.EncodeToString(hash.Sum(nil)),
-			Processed: false,
+			Hash:      fileHash,
+			Processed: fileIsProcessed,
 		}
-		i++
+		h.store[img.Hash] = img
+	}
+}
 
+func (h *imageHandlers) get(w http.ResponseWriter, r *http.Request) {
+	images := make([]Image, len(h.store))
+	i := 0
+
+	for _, image := range h.store {
+		images[i] = image
+		i++
 	}
 
 	jsonBytes, err := json.Marshal(images)
@@ -64,25 +86,81 @@ func (h *imageHandlers) get(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonBytes)
 }
 
-func (h *imageHandlers) post(w http.ResponseWriter, r *http.Request) {
+func (h *imageHandlers) show(w http.ResponseWriter, r *http.Request) {
+	parts := strings.Split(r.URL.String(), "/")
 
+	if len(parts) != 3 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	fmt.Print(parts[2])
+
+	fmt.Print("done")
+}
+
+func (h *imageHandlers) post(w http.ResponseWriter, r *http.Request) {
+	// parse form
+	r.ParseMultipartForm(32 << 20)
+
+	// check if already done from transmitted hash
+	if r.Form.Has("sha256") {
+		if image, ok := h.store[r.Form.Get("sha256")]; ok {
+			if image.Processed {
+				http.Redirect(w, r, "/image/"+image.Hash, http.StatusSeeOther)
+				return
+			}
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+	}
+
+	// read file
+	file, handler, err := r.FormFile("file")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	defer file.Close()
+
+	// create file
+	f, err := os.OpenFile("images/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	io.Copy(f, file)
+
+	// add to store
+	hash := sha256.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		panic(err)
+	}
+	fileHash := base64.RawURLEncoding.EncodeToString(hash.Sum(nil))
+	img := Image{
+		Name:      f.Name(),
+		Hash:      fileHash,
+		Processed: false,
+	}
+	h.store[img.Hash] = img
+
+	fmt.Print(img)
 }
 
 func newImageHandlers() *imageHandlers {
 	return &imageHandlers{
-		store: map[string]Image{
-			"id1": {
-				Name:      "hello.jpg",
-				Hash:      "fwfwefwefw11212",
-				Processed: false,
-			},
-		},
+		store: map[string]Image{},
 	}
 }
 
 func main() {
 	imageHandlers := newImageHandlers()
-	http.HandleFunc("/images", imageHandlers.get)
+	imageHandlers.loadStore()
+	http.HandleFunc("/", imageHandlers.get)
+	http.HandleFunc("/image", imageHandlers.post)
+	http.HandleFunc("/image/", imageHandlers.show)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
